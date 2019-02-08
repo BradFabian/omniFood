15311f3884a48ac28115f636a5671de19457fe1b
Brad <bradleofabian@gmail.com>
2019-02-07 23:16:11 -0500

    added sticky scroll navi in jquery

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    $(this).css("background-color", "#ff0000");
+  /* sticky Navi */
+  $(".js--section-features").waypoint(
+    function(direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "60px;"
+    }
+  );
+
+  /* Scroll Buttons */
+  $(".js--scroll-to-plans").click(function() {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-plans").offset().top },
+      1000
+    );
+  });
+
+  $(".js--scroll-to-start").click(function() {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-features").offset().top },
+      1000
+    );
   });
 });
